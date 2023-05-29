d877627aab3e7702618cd4ca5b6d8c2da4eefc16
Aditya Singh <singhaditya6774@gmail.com>
2023-05-29 21:34:29 +0530

    Create Camel casing pattern matching.cpp

---
+++ POTD{GFG}/Camel casing pattern matching.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    vector<string> CamelCase(int N, vector<string> Dictionary, string Pattern) {
+        vector<string> ans;
+        
+        for(int i=0; i<N; i++) {
+            string temp;
+            for(int j=0; j<Dictionary[i].size(); j++) {
+                if(isupper(Dictionary[i][j])) {
+                    temp.push_back(Dictionary[i][j]);
+                }
+                
+                if(temp == Pattern) {
+                    ans.push_back(Dictionary[i]);
+                    break;
+                }
+            }
+        }
+        
+        if(ans.size()) {
+            return ans;
+        }
+        else {
+            return {"-1"};
+    }
+    }
+};
